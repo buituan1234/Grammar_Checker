49984c687b9b67afafa232da43528a3825afab15
Jos-Dat <145478816+Jos-Dat@users.noreply.github.com>
2025-07-21 12:37:39 +0700

    Delete frontend directory

--- frontend/js/admin.js
+++
@@ -1,7 +0,0 @@
-  function logout() {
-    // Tùy chọn: clear localStorage nếu cần
-    // localStorage.removeItem("adminData"); // Nếu muốn xóa tài khoản admin
-
-    // Chuyển về trang login
-    window.location.href = "login.html";
-  }
